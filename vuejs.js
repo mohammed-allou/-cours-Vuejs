d103a2115838d0bfd4f234df175567f4ddb10a39
mallou <allou.mohammed.pro@gmail.com>
2020-05-04 20:00:35 +0100

    les Directives, les filtres, les Composants

--- vuejs.js
+++ vuejs.js
@@ -69,18 +69,145 @@
 // })
 
 // le systéme Watchers qui detecte lorsqu'une variable a été modifier 
-let vm = new Vue({
-	el:"#app",
-	data :{
-		firstname : 'momo',
-		lastname: 'pilou',
-		fullName : ' '
-	},
-	watch : {
-		fullName : function(value){
-			console.log('watch',value)
-		}
-	}
+// let vm = new Vue({
+// 	el:"#app",
+// 	data :{
+// 		firstname : 'momo',
+// 		lastname: 'pilou',
+// 		fullName : ' '
+// 	},
+// 	watch : {
+// 		fullName : function(value){
+// 			console.log('watch',value)
+// 		}
+// 	}
 	
 
+// })
+// mabghach yekhdem_______________________
+// let salam = function(el,binding){
+// 	el.value=binding.value
+// 	console.log('bind')
+// }
+
+// let vm = new Vue({
+// 	el:"#app",
+// 	directives:{
+// 		salam 
+// 	},
+// 	data :{
+// 		message: 'pilou',
+// 	},
+// 	methods : {
+// 		Demo : function(){
+// 			console.log('salam3lykoum')
+// 		},
+// 		demo2 : function(){
+// 			console.log('demo2')
+// 		}
+// 	}
+// })
+
+// //les filtres_____________________________________
+// Vue.filter('capitalize',function(value, prefix,suffix){
+// 	return prefix + value.toUpperCase() + suffix 
+// })
+
+// Vue.filter('reverse',function(value){
+// 	return value.split('').reverse().join('')
+// })
+// new Vue({
+// 	el : '#app',
+	
+// 	data :{
+// 		message : 'ramadan mobarak'
+// 	}
+// })
+//une autre maniére pour déclarer les filtres________
+
+// let capitalize =function(value, prefix,suffix){
+// 	return prefix + value.toUpperCase() + suffix 
+// }
+
+// new Vue({
+// 	el : '#app',
+// 	filters : {capitalize},	
+// 	data :{
+// 		message : 'ramadan mobarak'
+// 	}
+// })
+
+// les composants ____props un tableau_____________________________
+
+// Vue.component('message',{
+// 	props :[ "type","message"],
+// 	template : `<div class="ui message":class="type">{{message}}</div>`
+// })
+// let vm = new Vue({
+// 	el:"#app",
+// 	data :{
+// 	  message: 'salam 3alaykoum',
+// 	},
+// 	methods : {
+// 	  Demo : function(){
+// 	   console.log('salam3lykoum')
+// 	},
+// 	  demo2 : function(){
+// 	   console.log('demo2')
+// 	}
+// 	}
+// })
+
+// les composents __ props  un objet ________________________
+let message ={
+	props:{
+		type: {type: String, default: 'success'},
+		messege: String,
+		header: String
+
+		},
+	template: `<div class="ui message" :class="type">
+	<i class="close icon"></i>
+	<div class="header">{{ header }}</div>
+	{{message}}
+	</div>`
+}
+let counter ={
+	data: function(){
+		return {
+			count: 0
+		}
+	},
+	props: {
+		start:{type: Number,default:0}
+	},
+	methods: {
+		increment :function(){
+		this.count ++
+		}
+	},
+	
+	template: `<button @click="increment">{{ count }}</button>`,
+
+	mounted: function() {
+		this.count = this.start
+	}
+		// 	template: `<div>
+		// 	<span>{{ count }}</span><br>
+		// 	<button @click="increment">Incrémenter</button>
+		// 	</div>`
+ }
+let vm = new Vue({
+	el:"#app",
+	components: {message, counter},
+	data:{
+	 message: ' ',
+	 alert: false
+	},
+	methods:{
+		showAlert(){
+			this.alert = true
+		}
+	}
 })
+vm.message = 'salam 3alaykoum'
