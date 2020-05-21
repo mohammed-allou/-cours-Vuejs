28fd02755a1372b90bfefa786a1954bfaea4a696
mallou <allou.mohammed.pro@gmail.com>
2020-05-21 20:04:53 +0100

    test de todolist

--- todos1/src/App.vue
+++ todos1/src/App.vue
@@ -1,25 +1,52 @@
 <template>
   <div >
-    {{ todos }}
-    <todos v-model="todos"></todos>
-    <todos></todos>
-    <todos></todos>
-    <button @click="addTodo">Ajouter une tache</button>
+    <carousel>
+       <carousel-slide v-for"n in slides" :index="n-1">
+           <div style="position:absolute;left:0;right:0; text-align:center;top:50%;color:;font-size:15px;">
+             salut tous le monde
+           </div>
+           <img src="./components/carousel/logopsum1.jpg" width="30%">
+       </carousel-slide>
+       <carousel-slide>
+          <div style="position:absolute;left:0;right:0; text-align:center;top:50%;color:gray;font-size:15px;">
+               salam mes amis
+          </div>
+           <img src="./components/carousel/logopsum2.jpg" width="30%">
 
+       </carousel-slide>
+      
+   </carousel>
+   <button @click="addSlide">Ajouter un slide</button>
+   <button @click="removeSlide">Supprimer un slide</button>
+   
+    <todos v-model="todos"></todos>
   </div>
 </template>
 
 <script>
 import Todos from'./components/Todos'
+import carousel from './components/carousel/carousel'
+import carouselSlide from './components/carousel/carouselSlide'
 export default {
   
   data(){
     return{
+      slides:2,
       todos:[{
         name:'Demo',
         completed:true
       }]
     }
+  },
+  watch:{
+    slides(slides){
+      if(this.index >= this.slidesCount){
+        this.index=this.slidesCount -1
+      }
+    }
+  },
+  computed:{
+    slidesCount (){return this.slides.length}
   },
   methods:{
     addTodo() {
@@ -27,10 +54,17 @@ export default {
         name:'med',
         completed:false
       })
+    },
+    addSlide(){
+      this.slides++
+    },Slide(){
+      this.slides--
     }
   },
   components:{
-    Todos
+    Todos,
+    carousel,
+    carouselSlide
   }
 }
 </script>
---
+++ todos1/src/components/carousel/app.vue
@@ -0,0 +1,45 @@
+<template>
+  <div >
+   <carousel>
+       <carousel-slide>
+           salut lwaghech
+       </carousel-slide>
+       <carousel-slide>
+           salam lwaghech
+       </carousel-slide>
+   </carousel>
+    <todos v-model="todos"></todos>
+    
+
+  </div>
+</template>
+
+<script>
+import Todos from'./components/Todos'
+
+
+export default {
+  
+  data(){
+    return{
+      todos:[{
+        name:'Demo',
+        completed:true
+      }]
+    }
+  },
+  methods:{
+    addTodo() {
+      this.todos.push({
+        name:'med',
+        completed:false
+      })
+    }
+  },
+  components:{
+    Todos
+  }
+}
+</script>
+
+
